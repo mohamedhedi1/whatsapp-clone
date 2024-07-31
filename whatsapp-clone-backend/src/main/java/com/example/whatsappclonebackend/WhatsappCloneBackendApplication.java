7d0dff2c28f2830540e359d7aff5a2d5009a7ad9
Mohamed El Hedi Hamdi <mohamedhedi1hamdi@gmail.com>
2024-07-31 11:24:47 +0200

    initializing backend repo

---
+++ whatsapp-clone-backend/src/main/java/com/example/whatsappclonebackend/WhatsappCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsappclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappCloneBackendApplication.class, args);
+    }
+
+}
---
+++ whatsapp-clone-backend/src/test/java/com/example/whatsappclonebackend/WhatsappCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsappclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
